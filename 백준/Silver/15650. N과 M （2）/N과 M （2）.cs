4e3c822ce869df68696cab149192f66805d3b00f
Choi Jaewon <98089603+OneCrazyman@users.noreply.github.com>
2025-12-02 18:27:15 +0900

    [Silver III] Title: N과 M (2), Time: 52 ms, Memory: 5660 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.cs
@@ -0,0 +1,49 @@
+// 순열 생성
+// depth 있는 dfs로 가능할듯
+// m개만큼 최대 depth 조건 설정, 1~n중에 현재보다 큰 수들 전부로 dfs호출
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace algorithm_study
+{
+    class main
+    {
+        static int N,M;
+        static int[] sequence;
+        public static void Main(String[] args)
+        {
+            string[] input = Console.ReadLine().Split();
+            N = int.Parse(input[0]);
+            M = int.Parse(input[1]);
+            sequence = new int[M];
+            for (int i = 1; i <= N; i++)
+            {
+                DFS(i, 1);
+            }
+
+            return;
+        }
+
+        public static void DFS(int n, int depth) { 
+            sequence[depth-1] = n;
+            if (depth == M)
+            {
+                Print();
+                return;
+            }
+            for (int i = n + 1; i <= N; i++)
+            {
+                DFS(i, depth + 1);
+            }
+        }
+
+        public static void Print()
+        {
+            Console.WriteLine(string.Join(" ",sequence));
+        }
+    }
+}
